d4ec44d955dc423fd06192e33140799d14bfe687
Gerard Duch Jonama <g.duch@optimiz.me>
2021-09-08 17:47:59 +0200

    Finish react microfrontend

--- react-example/subA/src/App.js
+++ react-example/subA/src/App.js
@@ -1,3 +1,6 @@
-import React from 'react';
+import React from "react";
 
-export default () => <div>Sending love from Sub A</div>;
+const a = [1, 2, 3, 4];
+const func = (v) => <span> toto {v}</span>;
+
+export default () => <div>Sending love from Sub A {a.map(func)}</div>;
--- react-example/subA/src/index.js
+++ react-example/subA/src/index.js
@@ -1,5 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-ReactDOM.render(<App />, document.querySelector('#_sub-a-root'));
+const mount = (element) => ReactDOM.render(<App />, element);
+
+if (process.env.NODE_ENV === "development") {
+  const devRoot = document.querySelector("#_sub-a-root");
+  if (devRoot) {
+    mount(devRoot);
+  }
+}
+
+export { mount };
